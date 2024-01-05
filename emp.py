938036fddd8368b042c9582ab60fc03f8af00e61
rohan vetale <vetalerohan@gmail.com>
2024-01-05 14:18:40 +0530

    [Rohan] Add : Added daily wage calculation use case

--- emp.py
+++ emp.py
@@ -28,9 +28,27 @@ def is_present():
     
     return random.choice([True, False])
 
+def calc_daily_wage(wg_per_hr, hr_per_day):
+    """
+        Description:
+        This function is used to calculate daily wage of an employee
+
+        Parameter:
+        wg_per_hr : It is employee's per hour wage
+        hr_per_day : It is employee's per day working hours        
+
+        Return:
+        It returns daily wage of employee as integer value
+    """   
+    daily_wage = hr_per_day * wg_per_hr
+    return daily_wage
+     
+
 try:
     if is_present() :
         print("Employee is present today")
+        daily_wage = calc_daily_wage(wg_per_hr=20, hr_per_day=8)
+        print(f"Employee's daily wage is {daily_wage}")
     else:
         print("Employee is absent today")
         
